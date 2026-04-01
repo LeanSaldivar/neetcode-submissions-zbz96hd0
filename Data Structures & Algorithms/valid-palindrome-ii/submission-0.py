976718d0fb28069a28bd0d0fd842194d9ccb2666
Lean Saldivar <leanemmanuelsaldivar@gmail.com>
2026-04-01 13:27:40 +0800

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def isValid(l, r):
+            while l < r:
+                if s[l].lower() != s[r].lower():
+                    return False
+                l+=1
+                r-=1
+
+            return True
+        
+        l, r = 0, len(s)-1
+        while l < r:
+            if s[l].lower() != s[r].lower():
+                return (isValid(l + 1, r) or isValid(l, r-1))
+            l+=1
+            r-=1
+        return True
+
